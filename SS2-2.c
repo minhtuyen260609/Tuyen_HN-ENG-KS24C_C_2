736e83389e76e4ebd0cf3d44cf7bec580b1e784e
minhtuyen260609 <tuyendo260906@gmail.com>
2024-11-20 17:09:07 +0700

    Add files via upload

---
+++ SS2-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int firstNumber = 10;
+    int secondNumber = 20;
+    int sum = firstNumber + secondNumber;
+    printf("Tong la: %d", sum);
+
+    return 0;
+}
+
