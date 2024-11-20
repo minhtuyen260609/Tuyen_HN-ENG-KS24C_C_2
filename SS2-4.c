e0d3413c7aeeb9f44c7c1cb4866db44889fafe33
minhtuyen260609 <tuyendo260906@gmail.com>
2024-11-20 17:09:32 +0700

    Add files via upload

---
+++ SS2-4.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int canh = 7; 
+	int chu_vi = canh * 4; 
+	int dien_tich = canh * canh; 
+	printf("chu vi hinh vuong la:%d\n", chu_vi);
+	printf("dien tich hinh vuong la:%d", dien_tich); 
+	return 0; 
+} 
