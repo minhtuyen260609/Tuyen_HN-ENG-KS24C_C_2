8ca8e7488af717b892b21b63a70f77dd692a7516
minhtuyen260609 <tuyendo260906@gmail.com>
2024-11-20 17:09:21 +0700

    Add files via upload

---
+++ SS2-3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+	int firstNumber =30;
+	int secondNumber =10;
+	int sum = firstNumber + secondNumber;
+	int multip = firstNumber * secondNumber;
+	int subtraction = firstNumber - secondNumber;
+	int divide = firstNumber / secondNumber;
+	
+	printf("tong la: %d\n", sum);
+	printf("hieu la: %d\n", subtraction);
+	printf("tich la: %d\n", multip);
+	printf("thuong la: %d\n", divide);
+	return 0; 
+}
