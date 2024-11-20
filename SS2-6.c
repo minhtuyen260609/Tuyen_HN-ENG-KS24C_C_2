5e13f84564d8e5ef2d7dd09a1687dae658e0b7f3
minhtuyen260609 <tuyendo260906@gmail.com>
2024-11-20 17:09:59 +0700

    Add files via upload

---
+++ SS2-6.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int ban_kinh = 4;
+	float pi = 3.14;
+	float chu_vi = 2 * pi * ban_kinh;
+	float dien_tich = (chu_vi * chu_vi)/(4 * pi); 
+	printf("chu vi hinh tron la: %f", chu_vi);
+	printf("dien tich hinh tron la: %f", dien_tich); 
+	return 0; 
+} 
