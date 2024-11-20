19aede057d4eef6325998fe8d1825daf94970de3
minhtuyen260609 <tuyendo260906@gmail.com>
2024-11-20 17:09:45 +0700

    Add files via upload

---
+++ SS2-5.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int chieu_dai = 10;
+	int chieu_rong = 6;
+	int chu_vi = (chieu_dai + chieu_rong) * 2;
+	int dien_tich = chieu_dai * chieu_rong;
+	printf("chu vi hinh chu nhat la:%d\n",chu_vi);
+	printf("dien tich hinh chu nhat la:%d\n", dien_tich); 
+	return 0; 
+} 
