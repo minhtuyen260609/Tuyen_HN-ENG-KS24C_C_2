2f919238e891387fda9e2e1966fe1c224a8778a9
minhtuyen260609 <tuyendo260906@gmail.com>
2024-11-20 17:08:54 +0700

    Add files via upload

---
+++ SS2-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // int dung de luu giu cac gia tri là so nguyen 
+    int age = 19; 
+
+    // float dung de luu giu cac gia tri la so thap phan 
+    float height = 1.8;
+
+    // double dung de luu giu cac gia tri la so thap phan, gia tri chinh xac hon float 
+    double weight = 70.5;
+
+    // char dung de luu giu cac gia tri la ki tu 
+    char class = 'C';
+
+
+    return 0;
+}
+
